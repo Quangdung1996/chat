4dbf6a9a91e26e57e23ac31150952982db9dd9b3
Dung Bui <quangdung199696@gmail.com>
2025-11-06 16:50:44 +0700

    Fix

--- SourceAPI/SourceAPI/Services/RocketChat/RocketChatRoomService.cs
+++ SourceAPI/SourceAPI/Services/RocketChat/RocketChatRoomService.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Refit;
 using SourceAPI.Helpers.RocketChat;
 using SourceAPI.Infrastructure.Proxy;
 using SourceAPI.Models.RocketChat;
